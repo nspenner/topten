3abc634cefb3376ce7bb647ad42d78cc1ac60c45
Nathan Minchow <nathan.minchow@outlook.com>
2025-12-18 22:50:32 -0500

    Add repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/<REPO>/'
+  base: '/https://github.com/nspenner/topten/'
 })
